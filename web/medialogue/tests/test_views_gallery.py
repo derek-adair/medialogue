caefe3493c53f7b289184133889c0d384a67477f
Derek Adair <derek-adair@users.noreply.github.com>
2022-04-01 10:42:19 -0600

    More gallery to album

--- web/medialogue/tests/test_views_gallery.py
+++ web/medialogue/tests/test_views_gallery.py
@@ -8,22 +8,22 @@ class RequestAlbumTest(TestCase):
 
     def setUp(self):
         super(RequestAlbumTest, self).setUp()
-        self.gallery = AlbumFactory(slug='test-gallery')
+        self.album = AlbumFactory(slug='test-album')
 
-    def test_paginated_gallery_url_works(self):
-        response = self.client.get('/ptests/gallerylist/')
+    def test_paginated_album_url_works(self):
+        response = self.client.get('/ptests/albumlist/')
         self.assertEqual(response.status_code, 200)
 
-    def test_gallery_works(self):
-        response = self.client.get('/ptests/gallery/test-gallery/')
+    def test_album_works(self):
+        response = self.client.get('/ptests/album/test-album/')
         self.assertEqual(response.status_code, 200)
 
-    def test_detail_gallery_works(self):
-        response = self.client.get('/ptests/gallery/2011/12/23/test-gallery/')
+    def test_detail_album_works(self):
+        response = self.client.get('/ptests/album/2011/12/23/test-album/')
         self.assertEqual(response.status_code, 200)
 
     def test_redirect_to_list(self):
         """Trivial test - if someone requests the root url of the app
-        (i.e. /ptests/'), redirect them to the gallery list page."""
+        (i.e. /ptests/'), redirect them to the album list page."""
         response = self.client.get('/ptests/')
-        self.assertRedirects(response, '/ptests/gallery/', 301, 200)
+        self.assertRedirects(response, '/ptests/album/', 301, 200)
